44e556825d850712843850ee478bbca60a967312
JMKim <kjmdkdlel@nate.com>
2022-01-09 01:59:53 +0900

    Reduce the number of functions

--- v2/local/keyword_search_test.go
+++ v2/local/keyword_search_test.go
@@ -9,18 +9,19 @@ import (
 func TestKeyWordSearch(t *testing.T) {
 	query := "카카오프렌즈"
 	key := ""
-
-	if res, err := local.KeyWordSearch(query).
+	category_group_code := ""
+	x := "127.06283102249932"
+	y := "37.514322572335935"
+	radius := 20000
+	sort := "accuracy"
+	rect := ""
+	if res, err := local.KeyWordSearch(query, x, y, category_group_code, rect).
 		As(local.JSON).
 		AuthorizeWith(key).
-		SetCategoryGroupCode(local.DefaultCategoryGroupCode).
-		SetX(local.DefaultX).
-		SetY(local.DefaultY).
-		SetRadius(local.DefaultRadius).
-		SetRect(local.DefaultRect).
+		SetRadius(radius).
 		Result(local.DefaultPage).
 		Display(local.DefaultSize).
-		SortType(local.DefaultSort).
+		SortType(sort).
 		Collect(); err != nil {
 		t.Error(err)
 	} else {
