f9e5f23ad0fb5f223fe77cb0ca465b1dc619ee24
JMKim <kjmdkdlel@nate.com>
2022-01-12 00:48:20 +0900

    Fix Keyword Search and Transcoord

--- v2/local/keyword_search_test.go
+++ v2/local/keyword_search_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/maengsanha/kakao-developers-api/v2/local"
 )
 
-func TestKeyWordSearch(t *testing.T) {
+func TestKeywordSearchWithJSON(t *testing.T) {
 	query := "카카오프렌즈"
 	key := ""
 	category_group_code := ""
@@ -15,12 +15,44 @@ func TestKeyWordSearch(t *testing.T) {
 	radius := 20000
 	sort := "accuracy"
 	rect := ""
-	if res, err := local.KeyWordSearch(query, x, y, category_group_code, rect).
-		As(local.JSON).
+	if res, err := local.KeywordSearch(query).
+		As("json").
 		AuthorizeWith(key).
 		SetRadius(radius).
-		Result(local.DefaultPage).
-		Display(local.DefaultSize).
+		Result(1).
+		Display(15).
+		SetCategoryGroupCode(category_group_code).
+		SetX(x).
+		SetY(y).
+		SetRect(rect).
+		SortType(sort).
+		Collect(); err != nil {
+		t.Error(err)
+	} else {
+		t.Log(res)
+	}
+
+}
+
+func TestKeywordSearchWithXML(t *testing.T) {
+	query := "카카오프렌즈"
+	key := ""
+	category_group_code := ""
+	x := "127.06283102249932"
+	y := "37.514322572335935"
+	radius := 20000
+	sort := "accuracy"
+	rect := ""
+	if res, err := local.KeywordSearch(query).
+		As("xml").
+		AuthorizeWith(key).
+		SetRadius(radius).
+		Result(1).
+		Display(15).
+		SetCategoryGroupCode(category_group_code).
+		SetX(x).
+		SetY(y).
+		SetRect(rect).
 		SortType(sort).
 		Collect(); err != nil {
 		t.Error(err)
