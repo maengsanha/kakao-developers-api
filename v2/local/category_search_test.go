289d2bf785d69025180ce2cdde5c714d6348df2c
GoRyne <korsogogii@kookmin.ac.kr>
2022-01-12 17:49:31 +0900

    Fix iterator loop

--- v2/local/category_search_test.go
+++ v2/local/category_search_test.go
@@ -27,41 +27,51 @@ import (
 // PM9	약국
 
 func TestCategorySearchWithJSON(t *testing.T) {
-	key := ""
+	key := "e822b7f47b6b01317e771361317d2c38"
 	x := "127.06283102249932"
 	y := "37.514322572335935"
 	radius := 2000
 	categorygroupcode := "MT1"
 
-	if res, err := local.CategorySearch(categorygroupcode).
+	iter := local.CategorySearch(categorygroupcode).
 		FormatJSON().
 		AuthorizeWith(key).
 		SetLongitude(x).
 		SetLatitude(y).
 		SetRadius(radius).
 		Display(15).
-		Result(1).
-		Collect(); err != nil {
-		t.Error(err)
-	} else {
+		Result(1)
+
+	for res, err := iter.Next(); ; res, err = iter.Next() {
 		t.Log(res)
+		if err != nil {
+			if err != local.ErrEndPage {
+				t.Error(err)
+			}
+			break
+		}
 	}
 }
 
 func TestCategorySearchWithXML(t *testing.T) {
-	key := ""
+	key := "e822b7f47b6b01317e771361317d2c38"
 	rect := "1"
 	categorygroupcode := "MT1"
 
-	if res, err := local.CategorySearch(categorygroupcode).
+	iter := local.CategorySearch(categorygroupcode).
 		FormatXML().
 		AuthorizeWith(key).
 		SetRect(rect).
 		Display(15).
-		Result(1).
-		Collect(); err != nil {
-		t.Error(err)
-	} else {
+		Result(1)
+
+	for res, err := iter.Next(); ; res, err = iter.Next() {
 		t.Log(res)
+		if err != nil {
+			if err != local.ErrEndPage {
+				t.Error(err)
+			}
+			break
+		}
 	}
 }
