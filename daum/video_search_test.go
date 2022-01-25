83b10e6ac6b713bc77fab8c45ef6460b52d90643
GoRyne <korsogogii@kookmin.ac.kr>
2022-01-25 14:06:13 +0900

    Implement video)search

--- daum/video_search_test.go
+++ daum/video_search_test.go
@@ -1,1 +1,41 @@
 package daum_test
+
+import (
+	"testing"
+
+	"github.com/maengsanha/kakao-developers-client/daum"
+)
+
+func TestVideoSearchWithJSON(t *testing.T) {
+	query := "major scale code"
+
+	iter := daum.VideoSearch(query).
+		AuthorizeWith(daum.REST_API_KEY).
+		SortBy("accuracy").
+		Display(30).
+		Result(1)
+
+	for vr, err := iter.Next(); err == nil; vr, err = iter.Next() {
+		t.Log(vr)
+	}
+}
+
+func TestVideoSearchWithSaveAsJSON(t *testing.T) {
+	query := "minor code"
+
+	iter := daum.VideoSearch(query).
+		AuthorizeWith(daum.REST_API_KEY).
+		SortBy("accuracy").
+		Display(30).
+		Result(1)
+
+	vrs := daum.VideoSearchResults{}
+
+	for vr, err := iter.Next(); err == nil; vr, err = iter.Next() {
+		vrs = append(vrs, vr)
+	}
+
+	if err := vrs.SaveAs("video_search_test.json"); err != nil {
+		t.Error(err)
+	}
+}
