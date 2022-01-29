e99c22c14bb4c6dc8c30549e75105b0d6e702a4f
GoRyne <korsogogii@kookmin.ac.kr>
2022-01-29 17:32:19 +0900

    Implement translate

--- translation/translate_test.go
+++ translation/translate_test.go
@@ -1,1 +1,22 @@
 package translation_test
+
+import (
+	"internal/common"
+	"testing"
+
+	"github.com/maengsanha/kakao-developers-client/translation"
+)
+
+func TestTranslateWithJSON(t *testing.T) {
+	query := "화성학 5도 스케일"
+
+	if tr, err := translation.Translation(query).
+		Source("kr").
+		Target("en").
+		AuthorizeWith(common.REST_API_KEY).
+		Collect(); err != nil {
+		t.Error(err)
+	} else {
+		t.Log(tr)
+	}
+}
