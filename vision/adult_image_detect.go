e0dbfb4c6692f9f9af52f58d1f97524d906b17b2
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2022-02-07 23:59:52 +0900

    Implement thumbnail_create, thumbnail_detect, adult_image

--- vision/adult_image_detect.go
+++ vision/adult_image_detect.go
@@ -1,1 +1,103 @@
 package vision
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"internal/common"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+// AdultResult represents a document of a detected adult image result.
+// If the soft or adult score of an image is high, it is likely to be nudity or porn images. (normal + soft + adult = 1.0)
+type AdultResult struct {
+	Normal float64 `json:"normal"`
+	Soft   float64 `json:"soft"`
+	Adult  float64 `json:"adult"`
+}
+
+// AdultImageDetectResult represents a Adult Image Detection result.
+type AdultImageDetectResult struct {
+	Rid    string      `json:"rid"`
+	Result AdultResult `json:"result"`
+}
+
+// String implements fmt.Stringer.
+func (ar AdultImageDetectResult) String() string { return common.String(ar) }
+
+// SaveAs saves ar to @filename.
+//
+// The file extension must be .json.
+func (ar AdultImageDetectResult) SaveAs(filename string) error {
+	return common.SaveAsJSON(ar, filename)
+}
+
+// AdultImageDetectInitializer is a lazy adult image detector.
+type AdultImageDetectInitializer struct {
+	AuthKey  string
+	Image    *os.File
+	ImageUrl string
+}
+
+// AdultImageDetect determines the level of nudity or adult content in the given @source.
+//
+// Refer to https://developers.kakao.com/docs/latest/ko/vision/dev-guide#recog-adult-content for more details.
+func AdultImageDetect(source string) *AdultImageDetectInitializer {
+	url, file := CheckSourceType(source)
+	return &AdultImageDetectInitializer{
+		AuthKey:  common.KeyPrefix,
+		Image:    file,
+		ImageUrl: url,
+	}
+}
+
+// AuthorizeWith sets the authorization key to @key.
+func (ai *AdultImageDetectInitializer) AuthorizeWith(key string) *AdultImageDetectInitializer {
+	ai.AuthKey = common.FormatKey(key)
+	return ai
+}
+
+// Collect returns the adult image detection result.
+func (ai *AdultImageDetectInitializer) Collect() (res AdultImageDetectResult, err error) {
+	client := new(http.Client)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+
+	if ai.Image != nil {
+		part, err := writer.CreateFormFile("image", filepath.Base(ai.Image.Name()))
+		if err != nil {
+			return res, err
+		}
+		io.Copy(part, ai.Image)
+	}
+	defer writer.Close()
+
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/adult/detect?image_url=%s", prefix, ai.ImageUrl), body)
+	if err != nil {
+		return res, err
+	}
+	req.Close = true
+
+	req.Header.Set(common.Authorization, ai.AuthKey)
+	if ai.Image != nil {
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+	} else {
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	}
+	defer ai.Image.Close()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return res, err
+	}
+	defer resp.Body.Close()
+
+	if err = json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return res, err
+	}
+	return
+}
--- vision/thumbnail_create.go
+++ vision/thumbnail_create.go
@@ -1,1 +1,111 @@
 package vision
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"internal/common"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+// ThumbnailCreateInitializer is a lazy thumbnail creator.
+type ThumbnailCreateInitializer struct {
+	AuthKey  string
+	Image    *os.File
+	ImageUrl string
+	Width    int
+	Height   int
+}
+
+// ThumbnailCreateResult represents a Thumbnail creation result.
+type ThumbnailCreateResult struct {
+	ThumbnailImageUrl string `json:"thumbnail_image_url"`
+}
+
+// String implements fmt.Stringer.
+func (tr ThumbnailCreateResult) String() string { return common.String(tr) }
+
+// SaveAs saves tr to @filename.
+//
+// The file extension must be .json.
+func (tr ThumbnailCreateResult) SaveAs(filename string) error { return common.SaveAsJSON(tr, filename) }
+
+// ThumbnailCreate crops the representative area out of the given image and creates a thumbnail image.
+//
+// Refer to https://developers.kakao.com/docs/latest/ko/vision/dev-guide#create-thumbnail for more details.
+func ThumbnailCreate(source string) *ThumbnailCreateInitializer {
+	url, file := CheckSourceType(source)
+	return &ThumbnailCreateInitializer{
+		AuthKey:  common.KeyPrefix,
+		Image:    file,
+		ImageUrl: url,
+		Width:    0,
+		Height:   0,
+	}
+}
+
+// AuthorizeWith sets the authorization key to @key.
+func (ti *ThumbnailCreateInitializer) AuthorizeWith(key string) *ThumbnailCreateInitializer {
+	ti.AuthKey = common.FormatKey(key)
+	return ti
+}
+
+// WidthTo sets image width to @ratio.
+func (ti *ThumbnailCreateInitializer) WidthTo(ratio int) *ThumbnailCreateInitializer {
+	ti.Width = ratio
+	return ti
+}
+
+// Height sets image height to @ratio.
+func (ti *ThumbnailCreateInitializer) HeightTo(ratio int) *ThumbnailCreateInitializer {
+	ti.Height = ratio
+	return ti
+}
+
+// Collect returns the thumbnail creation result.
+func (ti *ThumbnailCreateInitializer) Collect() (res ThumbnailCreateResult, err error) {
+	client := new(http.Client)
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+
+	if ti.Image != nil {
+		part, err := writer.CreateFormFile("image", filepath.Base(ti.Image.Name()))
+
+		if err != nil {
+			return res, err
+		}
+		io.Copy(part, ti.Image)
+	}
+	defer writer.Close()
+
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/thumbnail/crop?image_url=%s&width=%d&height=%d",
+		prefix, ti.ImageUrl, ti.Width, ti.Height), body)
+	if err != nil {
+		return res, err
+	}
+	req.Close = true
+	req.Header.Set(common.Authorization, ti.AuthKey)
+	if ti.Image != nil {
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+	} else {
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	}
+	defer ti.Image.Close()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return res, err
+	}
+
+	defer resp.Body.Close()
+
+	if err = json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return res, err
+	}
+	return
+
+}
