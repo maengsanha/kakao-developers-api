e1de3bb24e334dbfbf50abbb8474eef0233c4a4a
GoRyne <korsogogii@kookmin.ac.kr>
2022-02-15 17:33:28 +0900

    Implements video analysis result

---
+++ pose/video_analysis_result_test.go
@@ -0,0 +1,32 @@
+package pose_test
+
+import (
+	"internal/common"
+	"testing"
+
+	"github.com/maengsanha/kakao-developers-client/pose"
+)
+
+func TestVideoAnalyzeResult(t *testing.T) {
+	job_id := "9524567f-887b-474f-9e33-a3d480b400c1"
+
+	if cr, err := pose.CheckVideo(job_id).
+		AuthorizeWith(common.REST_API_KEY).
+		Collect(); err != nil {
+		t.Error(err)
+	} else {
+		t.Log(cr)
+	}
+}
+
+func TestVideoAnalyzeResultSaveAsJSON(t *testing.T) {
+	job_id := "9524567f-887b-474f-9e33-a3d480b400c1"
+
+	if cr, err := pose.CheckVideo(job_id).
+		AuthorizeWith(common.REST_API_KEY).
+		Collect(); err != nil {
+		t.Error(err)
+	} else if err := cr.SaveAs("video_analysis_result_test.json"); err != nil {
+		t.Error(cr)
+	}
+}
