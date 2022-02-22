cdc8a914e4c280691a03748724029ace15b6b634
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2022-02-22 19:21:26 +0900

    Modify comments

--- vision/ocr.go
+++ vision/ocr.go
@@ -3,10 +3,9 @@ package vision
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
+	"errors"
 	"internal/common"
 	"io"
-	"log"
 	"mime/multipart"
 	"net/http"
 	"os"
@@ -22,16 +21,16 @@ type OCRInitializer struct {
 
 // Result represents a document of a Optical Character Recognition result.
 type Result struct {
-	Boxes            [][]int  `json:"boxes"`
-	RecognitionWords []string `json:"recognition_words"`
+	Boxes            [][]float64 `json:"boxes"`
+	RecognitionWords []string    `json:"recognition_words"`
 }
 
-// OCRResult represents a Optical Character Recognition result.
+// OCRResult represents an Optical Character Recognition result.
 type OCRResult struct {
 	Result Result `json:"result"`
 }
 
-// SaveAs saves or to @filename
+// SaveAs saves or to @filename.
 //
 // The file extension must be .json.
 func (or OCRResult) SaveAs(filename string) error { return common.SaveAsJSON(or, filename) }
@@ -41,27 +40,21 @@ func (or OCRResult) String() string { return common.String(or) }
 
 // OCR detects and extracts text from the given @filepath.
 //
-// @filepath must be image file path.
-// file format must be one of the BMP, DIB, JPEG, JPE, JP2, WEBP, PBM, PGM, PPM, SR, RAS, TIFF, TIF, PNG and JPG.
+// File format must be one of the BMP, DIB, JPEG, JPE, JP2, WEBP, PBM, PGM, PPM, SR, RAS, TIFF, TIF, PNG and JPG.
 // Refer to https://developers.kakao.com/docs/latest/ko/vision/dev-guide#ocr for more details.
 func OCR(filepath string) *OCRInitializer {
 	switch format := strings.Split(filepath, "."); format[len(format)-1] {
 	case "bmp", "dib", "jpeg", "jpg", "jpe", "jp2", "png", "webp",
 		"pbm", "pgm", "ppm", "sr", "ras", "tiff", "tif":
-		break
 	default:
-		panic(ErrUnsupportedFormat)
-	}
-	if r := recover(); r != nil {
-		log.Panicln(r)
+		panic(errors.New("unsupport format"))
 	}
 
 	file, err := os.Open(filepath)
+
 	if err != nil {
-		log.Println(err)
+		panic(errors.New("invalid file path"))
 	}
-	CheckFileSize(file)
-	CheckImagePixel(filepath)
 	return &OCRInitializer{
 		AuthKey: common.KeyPrefix,
 		Image:   file,
@@ -75,36 +68,38 @@ func (oi *OCRInitializer) AuthorizeWith(key string) *OCRInitializer {
 }
 
 // Collect returns the OCR result.
-func (oi *OCRInitializer) Collect() (res OCRResult, err error) {
+func (oi *OCRInitializer) Collect() (res json.RawMessage, err error) {
 	client := new(http.Client)
-
-	body := &bytes.Buffer{}
+	body := new(bytes.Buffer)
 	bodywriter := multipart.NewWriter(body)
-
 	filewriter, err := bodywriter.CreateFormFile("image", filepath.Base(oi.Image.Name()))
+
 	if err != nil {
 		return res, err
 	}
 	io.Copy(filewriter, oi.Image)
+	defer bodywriter.Close()
 
-	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/text/ocr", prefix), body)
+	req, err := http.NewRequest(http.MethodPost, "https://dapi.kakao.com/v2/vision/text/ocr", body)
 	if err != nil {
 		return res, err
 	}
-	req.Close = true
+
 	req.Header.Set(common.Authorization, oi.AuthKey)
 	req.Header.Set("Content-Type", "multipart/form-data")
 
+	defer req.Body.Close()
 	resp, err := client.Do(req)
 	if err != nil {
 		return res, err
 	}
+
 	defer resp.Body.Close()
-	defer bodywriter.Close()
 	defer oi.Image.Close()
 
 	if err = json.NewDecoder(resp.Body).Decode(&res); err != nil {
 		return res, err
 	}
+
 	return
 }
